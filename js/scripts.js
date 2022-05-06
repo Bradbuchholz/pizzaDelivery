96515f200077c2ad505679da05da71877124bcca
Bradbuchholz <bradbuchholz@gmail.com>
2022-05-06 16:54:27 -0700

    Add pizza.prototype functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var Pizza = (forName, size, cheese, pepperoni, sausage, ham, bacon, mushrooms, onions, olives, peppers, ranchDrizzle) {
-  this.forName = forName;
+this.forName = forName;
 this.size = size;
 this.cheese = cheese;
 this.pepperoni = pepperoni;
@@ -23,7 +23,101 @@ Pizza.prototype.pricing = function() {
     totalPrice = totalPrice + 13;
   } else if (this.size === 4) {
     totalPrice = totalPrice + 16;
+  }
   
-  } if 
+  if (this.cheese === 1) {
+    totalPrice = totalPrice + 1;
+  } else if (this.cheese === 2) {
+    totalPrice = totalPrice + 2;
+  }
+  
+  if (this.pepperoni === 1) {
+    totalPrice = totalPrice + 0;
+  } else if (this.pepperoni === 2) {
+    totalPrice = totalPrice + 2;
+  } else if (this.pepperoni === 3) {
+    totalPrice = totalPrice +3;
+  }
+
+  if (this.sausage === 1) {
+    totalPrice = totalPrice + 0;
+  } else if (this.sausage === 2) {
+    totalPrice = totalPrice + 2;
+  } else if (this.sausage === 3) {
+    totalPrice = totalPrice + 3;
+  }
+
+  if (this.ham === 1) {
+    totalPrice = totalPrice + 0;
+  } else if (this.ham === 2) {
+    totalPrice = totalPrice + 2;
+  } else if (this.ham === 3) {
+    totalPrice = totalPrice + 3;
+  }
+
+  if (this.bacon === 1) {
+    totalPrice = totalPrice + 0;
+  } else if (this.bacon === 2) {
+    totalPrice = totalPrice + 2;
+  } else if (this.bacon === 3) {
+    totalPrice = totalPrice + 3;
+  }
+
+  if (this.mushrooms === 1) {
+    totalPrice = totalPrice + 0;
+  } else if (this.mushrooms === 2) {
+    totalPrice = totalPrice + 2;
+  } else if (this.mushrooms === 3) {
+    totalPrice = totalPrice + 3;
+  }
+
+  if (this.onions === 1) {
+    totalPrice = totalPrice + 0;
+  } else if (this.onions === 2) {
+    totalPrice = totalPrice + 2;
+  } else if (this.onions === 3) {
+    totalPrice = totalPrice + 3;
+  }
+
+  if (this.olives === 1) {
+    totalPrice = totalPrice + 0; 
+  } else if (this.olives === 2) {
+    totalPrice === totalPrice + 2;
+  } else if (this.olives === 3) {
+    totalPrice = totalPrice + 3;
+  }
+
+  if (this.peppers === 1) {
+    totalPrice = totalPrice + 0;
+  } else if (this.peppers === 2) {
+    totalPrice = totalPrice + 2;
+  } else if (this.peppers === 3) {
+    totalPrice = totalPrice + 3;
+  }
+
+  if (this.ranchDrizzle === 1) {
+    totalPrice = totalPrice + 0;
+  } else if (this.ranchDrizzle === 2) {
+    totalPrice = totalPrice + 2;
+  } else if (this.ranchDrizzle === 3) {
+    totalPrice = totalPrice + 3;
+  }
+
+  return totalPrice;
 }
 
+Pizza.prototype.toppings = function() {
+  var toppingsArray = [];
+
+  if (this.size === 1) {
+    toppingsArray.push("Small");
+  } else if (this.size === 2) {
+    toppingsArray.push("Medium");
+  } else if (this.size === 3) {
+    toppingsArray.push("Large");
+  } else if (this.size === 4) {
+    toppingsArray.push("Extra Large");
+  }
+
+  
+}

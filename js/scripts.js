7dcd6cfec12860825e892c19f03e3f642b24bb79
Bradbuchholz <bradbuchholz@gmail.com>
2022-05-07 18:04:17 -0700

    finish toppingsArray branching

--- js/scripts.js
+++ js/scripts.js
@@ -119,5 +119,85 @@ Pizza.prototype.toppings = function() {
     toppingsArray.push("Extra Large");
   }
 
-  
+  if (this.cheese === 1) {
+    toppingsArray.push("Regular cheese");
+  } else if (this.cheese === 2) {
+    toppingsArray.push("Extra cheese");
+  } else if (this.cheese === 3) {
+    toppingsArray.push("Phteve");
+  }
+
+  if (this.pepperoni === 1) {
+    toppingsArray.push("");
+  } else if (this.pepperoni === 2) {
+    toppingsArray.push("Regular Pepperoni")
+  } else if (this.pepperoni === 3) {
+    toppingsArray.push("Extra Pepperoni");
+  }
+
+  if (this.sausage === 1) {
+    toppingsArray.push("");
+  } else if (this.sausage === 2) {
+    toppingsArray.push("Regular Sausage")
+  } else if (this.sausage === 3) {
+    toppingsArray.push("Extra Sausage");
+  }
+
+  if (this.ham === 1) {
+    toppingsArray.push("");
+  } else if (this.ham === 2) {
+    toppingsArray.push("Regular Ham")
+  } else if (this.ham === 3) {
+    toppingsArray.push("Extra Ham");
+  }
+
+  if (this.bacon === 1) {
+    toppingsArray.push("");
+  } else if (this.bacon === 2) {
+    toppingsArray.push("Regular Bacon")
+  } else if (this.bacon === 3) {
+    toppingsArray.push("Extra Bacon");
+  }
+
+  if (this.mushrooms === 1) {
+    toppingsArray.push("");
+  } else if (this.mushrooms === 2) {
+    toppingsArray.push("Regular Mushrooms")
+  } else if (this.mushrooms === 3) {
+    toppingsArray.push("Extra Mushrooms");
+  }
+
+  if (this.onions === 1) {
+    toppingsArray.push("");
+  } else if (this.onions === 2) {
+    toppingsArray.push("Regular Onions")
+  } else if (this.onions === 3) {
+    toppingsArray.push("Extra Onions");
+  }
+
+  if (this.olives === 1) {
+    toppingsArray.push("");
+  } else if (this.olives === 2) {
+    toppingsArray.push("Regular Olives")
+  } else if (this.olives === 3) {
+    toppingsArray.push("Extra Olives");
+  }
+
+  if (this.peppers === 1) {
+    toppingsArray.push("");
+  } else if (this.peppers === 2) {
+    toppingsArray.push("Regular Peppers")
+  } else if (this.peppers === 3) {
+    toppingsArray.push("Extra Peppers");
+  }
+
+  if (this.ranchDrizzle === 1) {
+    toppingsArray.push("");
+  } else if (this.ranchDrizzle === 2) {
+    toppingsArray.push("Regular Ranch Drizzle")
+  } else if (this.ranchDrizzle === 3) {
+    toppingsArray.push("Extra Ranch Drizzle");
+  }
+  return toppingsArray;
 }
+
